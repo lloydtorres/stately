2c3059c10404c60d8f350fde7060537e3f591c4b
lloydtorres <lloyd.torres95@gmail.com>
2016-01-27 19:55:22 -0500

    Create settings activity

---
+++ Stately/app/src/main/java/com/lloydtorres/stately/settings/SettingsFragment.java
@@ -0,0 +1,28 @@
+package com.lloydtorres.stately.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lloydtorres.stately.R;
+
+/**
+ * Created by Lloyd on 2016-01-27.
+ * The fragment within the Settings activity.
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        return view;
+    }
+}
