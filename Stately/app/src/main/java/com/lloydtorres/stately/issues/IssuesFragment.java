c6c692527bee829fbe36b2fbf8e6919718eb4b5f
lloydtorres <lloyd.torres95@gmail.com>
2016-01-28 22:34:30 -0500

    Create issues fragment and connect to NS issues

--- Stately/app/src/main/java/com/lloydtorres/stately/issues/IssuesFragment.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/issues/IssuesFragment.java
@@ -13,15 +13,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.lloydtorres.stately.R;
+import com.lloydtorres.stately.dto.Issue;
 import com.lloydtorres.stately.helpers.PrimeActivity;
 import com.lloydtorres.stately.helpers.SparkleHelper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lloyd on 2016-01-28.
  * A fragment to display current issues.
  */
 public class IssuesFragment extends Fragment {
+    public static final String BASE_URI = "https://www.nationstates.net/";
+    private static final String UNADDRESSED = "unaddressed";
+    private static final String PENDING = "legislation pending";
+    private static final String DISMISSED = "dismissed";
+
     private Activity mActivity;
     private View mView;
     private Toolbar toolbar;
@@ -31,6 +54,8 @@ public class IssuesFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mRecyclerAdapter;
+
+    private List<Issue> issues;
 
     @Override
     public void onAttach(Context context) {
@@ -61,11 +86,11 @@ public class IssuesFragment extends Fragment {
         mSwipeRefreshLayout = (SwipeRefreshLayout) mView.findViewById(R.id.issues_refresher);
         mSwipeRefreshLayout.setColorSchemeResources(SparkleHelper.refreshColours);
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-                                                    @Override
-                                                    public void onRefresh() {
-                                                        // @TODO
-                                                    }
-                                                });
+            @Override
+            public void onRefresh() {
+                queryIssues(mView);
+            }
+        });
 
         mFloatingActionButton = (FloatingActionButton) mView.findViewById(R.id.issues_fab);
 
@@ -82,8 +107,91 @@ public class IssuesFragment extends Fragment {
                 mSwipeRefreshLayout.setRefreshing(true);
             }
         });
+        queryIssues(mView);
 
         return mView;
+    }
+
+    /**
+     * Scrape the issues from the actual NationStates site
+     * @param view
+     */
+    private void queryIssues(final View view)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this.getActivity());
+        String targetURL = Issue.QUERY;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, targetURL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Document d = Jsoup.parse(response, BASE_URI);
+                        processIssues(d);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                SparkleHelper.logError(error.toString());
+                mSwipeRefreshLayout.setRefreshing(false);
+                if (error instanceof TimeoutError || error instanceof NoConnectionError || error instanceof NetworkError) {
+                    SparkleHelper.makeSnackbar(view, getString(R.string.login_error_no_internet));
+                }
+                else
+                {
+                    SparkleHelper.makeSnackbar(view, getString(R.string.login_error_generic));
+                }
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+    /**
+     * Process the HTML contents of the issues into actual Issue objects
+     * @param d
+     */
+    private void processIssues(Document d)
+    {
+        issues = new ArrayList<Issue>();
+
+        Element issuesContainer = d.select("ul.dilemmalist").first();
+        Elements issuesRaw = issuesContainer.children();
+
+        for (Element i : issuesRaw)
+        {
+            Issue issueCore = new Issue();
+
+            Elements issueContents = i.children();
+
+            // Get issue ID and name
+            Element issueMain = issueContents.select("a").first();
+            String issueLink = issueMain.attr("href");
+            int issueId = Integer.valueOf(issueLink.replace("page=show_dilemma/dilemma=", ""));
+            issueCore.id = issueId;
+            String issueName = issueMain.text();
+            issueCore.title = issueName;
+
+            // Get issue status
+            String issueStat = i.text().trim().replaceAll(".* \\[(.*?)\\]", "$1").toLowerCase();
+            switch (issueStat)
+            {
+                case PENDING:
+                    issueCore.status = Issue.STATUS_PENDING;
+                    break;
+                case DISMISSED:
+                    issueCore.status = Issue.STATUS_DISMISSED;
+                    break;
+                default:
+                    issueCore.status = Issue.STATUS_UNADDRESSED;
+                    break;
+            }
+
+            issues.add(issueCore);
+        }
+
+        mRecyclerAdapter = new IssuesRecyclerAdapter(getContext(), issues);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
---
+++ Stately/app/src/main/java/com/lloydtorres/stately/issues/IssuesRecyclerAdapter.java
@@ -0,0 +1,86 @@
+package com.lloydtorres.stately.issues;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lloydtorres.stately.R;
+import com.lloydtorres.stately.dto.Issue;
+
+import java.util.List;
+
+/**
+ * Created by Lloyd on 2016-01-28.
+ * An adapter for the IssuesFragment recycler.
+ */
+public class IssuesRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+    private List<Issue> issues;
+
+    public IssuesRecyclerAdapter(Context c, List<Issue> i)
+    {
+        context = c;
+        issues = i;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View issueCard = inflater.inflate(R.layout.card_issue_main, parent, false);
+        RecyclerView.ViewHolder viewHolder = new IssueCard(context, issueCard);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        IssueCard issueCard = (IssueCard) holder;
+        issueCard.init(issues.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return issues.size();
+    }
+
+    public class IssueCard extends RecyclerView.ViewHolder {
+
+        private Context context;
+        private TextView title;
+        private TextView state;
+        private ImageView stateIcon;
+
+        public IssueCard(Context c, View v) {
+            super(v);
+            context = c;
+            title = (TextView) v.findViewById(R.id.card_issue_main_title);
+            state = (TextView) v.findViewById(R.id.card_issue_main_status);
+            stateIcon = (ImageView) v.findViewById(R.id.card_issue_main_status_icon);
+        }
+
+        public void init(Issue i)
+        {
+            title.setText(i.title);
+            switch(i.status)
+            {
+                case Issue.STATUS_PENDING:
+                    state.setText(context.getString(R.string.issue_pending));
+                    stateIcon.setImageResource(R.drawable.ic_check_grey);
+                    break;
+                case Issue.STATUS_DISMISSED:
+                    state.setText(context.getString(R.string.issue_dismissed));
+                    stateIcon.setImageResource(R.drawable.ic_dismiss_grey);
+                    break;
+                default:
+                    state.setText(context.getString(R.string.issue_unaddressed));
+                    state.setTextColor(ContextCompat.getColor(context, R.color.issueUnaddressed));
+                    stateIcon.setImageResource(R.drawable.ic_unaddressed);
+                    break;
+            }
+        }
+    }
+}
