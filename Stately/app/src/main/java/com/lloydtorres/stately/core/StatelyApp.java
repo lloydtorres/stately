f220bbd09026f3df39151942b7f2c891e59fbed8
lloydtorres <lloyd.torres95@gmail.com>
2016-02-03 19:21:47 -0500

    Integrate SugarORM

--- Stately/app/src/main/java/com/lloydtorres/stately/core/StatelyApp.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/core/StatelyApp.java
@@ -1,10 +1,10 @@
 package com.lloydtorres.stately.core;
 
-import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.crashlytics.android.Crashlytics;
+import com.orm.SugarApp;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -12,7 +12,7 @@ import io.fabric.sdk.android.Fabric;
  * Created by Lloyd on 2016-01-29.
  * This class is used for app-wide changes.
  */
-public class StatelyApp extends Application {
+public class StatelyApp extends SugarApp {
     private SharedPreferences storage; // shared preferences
 
     @Override
