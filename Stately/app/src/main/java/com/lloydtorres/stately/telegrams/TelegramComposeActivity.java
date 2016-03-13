1e6d9ced9c9d60b0316f7ed89a02bd2f3bb6499d
lloydtorres <lloyd.torres95@gmail.com>
2016-03-12 23:32:54 -0500

    Add ability to send telegrams

--- Stately/app/src/main/java/com/lloydtorres/stately/telegrams/TelegramComposeActivity.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/telegrams/TelegramComposeActivity.java
@@ -16,7 +16,9 @@
 
 package com.lloydtorres.stately.telegrams;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -24,12 +26,34 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.common.base.Joiner;
 import com.lloydtorres.stately.R;
+import com.lloydtorres.stately.dto.Telegram;
+import com.lloydtorres.stately.dto.UserLogin;
+import com.lloydtorres.stately.helpers.DashHelper;
 import com.lloydtorres.stately.helpers.NullActionCallback;
 import com.lloydtorres.stately.helpers.SparkleHelper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Lloyd on 2016-03-12.
@@ -40,11 +64,14 @@ public class TelegramComposeActivity extends AppCompatActivity {
     public static final String REPLY_ID_DATA = "replyIdData";
     public static final String RECIPIENTS_DATA = "recipientsData";
     public static final int NO_REPLY_ID = -1;
+    private static final String SENT_CONFIRM_1 = "Your telegram is being wired";
+    private static final String SENT_CONFIRM_2 = "Your telegram has been wired";
 
     private int replyId = NO_REPLY_ID;
     private String recipients;
 
     private View mView;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private EditText recipientsField;
     private TextView senderField;
     private EditText content;
@@ -61,6 +88,9 @@ public class TelegramComposeActivity extends AppCompatActivity {
         }
 
         mView = findViewById(R.id.telegram_compose_main);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.telegram_compose_refresher);
+        mSwipeRefreshLayout.setColorSchemeResources(SparkleHelper.refreshColours);
+        mSwipeRefreshLayout.setEnabled(false);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.telegram_compose_toolbar);
         setToolbar(toolbar);
@@ -96,6 +126,214 @@ public class TelegramComposeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+    /**
+     * This makes sure that the recipients entered by the user are valid.
+     */
+    private void checkTelegramRecipients()
+    {
+        // Clear focus then hide keyboard
+        recipientsField.clearFocus();
+        content.clearFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mView.getWindowToken(), 0);
+
+        // Sanity checks
+        String recipientsRaw = recipientsField.getText().toString();
+        if (recipientsRaw.length() <= 0)
+        {
+            SparkleHelper.makeSnackbar(mView, getString(R.string.telegrams_empty_recipients));
+            return;
+        }
+
+        String contentRaw = content.getText().toString();
+        if (contentRaw.length() <= 0)
+        {
+            SparkleHelper.makeSnackbar(mView, getString(R.string.telegrams_empty_content));
+            return;
+        }
+
+        // Check if recipients are valid names
+        List<String> recipientIds = new ArrayList<String>();
+        if (recipientsRaw.contains(","))
+        {
+            List<String> recipientItems = Arrays.asList(recipientsRaw.split(","));
+            for (String r : recipientItems)
+            {
+                String rawRecipient = r.trim();
+                if (SparkleHelper.isValidName(rawRecipient) && rawRecipient.length() > 0)
+                {
+                    recipientIds.add(SparkleHelper.getIdFromName(rawRecipient));
+                }
+                else
+                {
+                    SparkleHelper.makeSnackbar(mView, getString(R.string.telegrams_invalid_recipient));
+                    return;
+                }
+            }
+        }
+        else
+        {
+            String recipientCheck = recipientsRaw.trim();
+            if (SparkleHelper.isValidName(recipientCheck) && recipientCheck.length() > 0)
+            {
+                recipientIds.add(SparkleHelper.getIdFromName(recipientCheck));
+            }
+            else
+            {
+                SparkleHelper.makeSnackbar(mView, getString(R.string.telegrams_invalid_recipient));
+                return;
+            }
+        }
+
+        startTelegramSend(recipientIds);
+    }
+
+    /**
+     * Helper for starting the telegram sending process.
+     * @param recipients List of verified recipients
+     */
+    private void startTelegramSend(final List<String> recipients)
+    {
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                getTelegramCheckValue(recipients);
+            }
+        });
+    }
+
+    /**
+     * Gets the check value needed to send a telegram.
+     * @param recipients List of verified recipients
+     */
+    private void getTelegramCheckValue(final List<String> recipients)
+    {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, Telegram.SEND_TELEGRAM,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Document d = Jsoup.parse(response, SparkleHelper.BASE_URI);
+                        Element input = d.select("input[name=chk]").first();
+
+                        if (input == null)
+                        {
+                            mSwipeRefreshLayout.setRefreshing(false);
+                            SparkleHelper.makeSnackbar(mView, getString(R.string.login_error_parsing));
+                            return;
+                        }
+
+                        String chk = input.attr("value");
+                        sendTelegram(recipients, chk);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                SparkleHelper.logError(error.toString());
+                mSwipeRefreshLayout.setRefreshing(false);
+                if (error instanceof TimeoutError || error instanceof NoConnectionError || error instanceof NetworkError) {
+                    SparkleHelper.makeSnackbar(mView, getString(R.string.login_error_no_internet));
+                }
+                else
+                {
+                    SparkleHelper.makeSnackbar(mView, getString(R.string.login_error_generic));
+                }
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String,String> params = new HashMap<String, String>();
+                UserLogin u = SparkleHelper.getActiveUser(getApplicationContext());
+                params.put("User-Agent", String.format(getString(R.string.app_header), u.nationId));
+                params.put("Cookie", String.format("autologin=%s", u.autologin));
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        if (!DashHelper.getInstance(this).addRequest(stringRequest))
+        {
+            mSwipeRefreshLayout.setRefreshing(false);
+            SparkleHelper.makeSnackbar(mView, getString(R.string.rate_limit_error));
+        }
+    }
+
+    private void sendTelegram(final List<String> recipients, final String chk)
+    {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Telegram.SEND_TELEGRAM,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        mSwipeRefreshLayout.setRefreshing(false);
+                        String textResponse = Jsoup.parse(response, SparkleHelper.BASE_URI).text();
+                        if (textResponse.contains(SENT_CONFIRM_1) || textResponse.contains(SENT_CONFIRM_2))
+                        {
+                            finish();
+                        }
+                        else
+                        {
+                            SparkleHelper.makeSnackbar(mView, getString(R.string.telegrams_fail));
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                SparkleHelper.logError(error.toString());
+                mSwipeRefreshLayout.setRefreshing(false);
+                if (error instanceof TimeoutError || error instanceof NoConnectionError || error instanceof NetworkError) {
+                    SparkleHelper.makeSnackbar(mView, getString(R.string.login_error_no_internet));
+                }
+                else
+                {
+                    SparkleHelper.makeSnackbar(mView, getString(R.string.login_error_generic));
+                }
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("chk", chk);
+                params.put("tgto", Joiner.on(", ").skipNulls().join(recipients));
+                if (replyId == NO_REPLY_ID)
+                {
+                    params.put("recruitregion", "region");
+                    params.put("recruitregionrealname", SparkleHelper.getRegionSessionData(getApplicationContext()));
+                    params.put("send", "1");
+                }
+                else
+                {
+                    params.put("in_reply_to", String.valueOf(replyId));
+                    if (recipients.size() > 1)
+                    {
+                        params.put("send_to_all", "1");
+                    }
+                    else
+                    {
+                        params.put("send", "1");
+                    }
+                }
+                params.put("message", content.getText().toString());
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String,String> params = new HashMap<String, String>();
+                UserLogin u = SparkleHelper.getActiveUser(getApplicationContext());
+                params.put("User-Agent", String.format(getString(R.string.app_header), u.nationId));
+                params.put("Cookie", String.format("autologin=%s", u.autologin));
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        if (!DashHelper.getInstance(this).addRequest(stringRequest))
+        {
+            mSwipeRefreshLayout.setRefreshing(false);
+            SparkleHelper.makeSnackbar(mView, getString(R.string.rate_limit_error));
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -111,7 +349,7 @@ public class TelegramComposeActivity extends AppCompatActivity {
                 finish();
                 return true;
             case R.id.nav_send_telegram:
-                // @TODO Send telegram
+                checkTelegramRecipients();
                 return true;
         }
         return super.onOptionsItemSelected(item);
