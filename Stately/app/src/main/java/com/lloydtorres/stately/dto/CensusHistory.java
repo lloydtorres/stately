022b84f24e45ad3545eaea7e43c7a7f8287b1fcd
lloydtorres <lloyd.torres95@gmail.com>
2016-04-10 02:37:07 -0400

    Create models for census history data

---
+++ Stately/app/src/main/java/com/lloydtorres/stately/dto/CensusHistory.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2016 Lloyd Torres
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lloydtorres.stately.dto;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.lloydtorres.stately.helpers.SparkleHelper;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+/**
+ * Created by Lloyd on 2016-04-10.
+ * Model for a given census scale's history;
+ */
+@Root(name="NATION", strict=false)
+public class CensusHistory implements Parcelable {
+
+    public static final String QUERY = "https://www.nationstates.net/cgi-bin/api.cgi?%s=%s&q="
+                                        + "name+census"
+                                        + ";scale=%d;mode=history"
+                                        + ";from=%d&to=%d"
+                                        + "&v=" + SparkleHelper.API_VERSION;
+
+    @Element(name="NAME")
+    public String name;
+    @Element(name="CENSUS")
+    public CensusHistoryHolder censusHolder;
+
+    public CensusHistory() { super(); }
+
+    protected CensusHistory(Parcel in) {
+        name = in.readString();
+        censusHolder = (CensusHistoryHolder) in.readValue(CensusHistoryHolder.class.getClassLoader());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeValue(censusHolder);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<CensusHistory> CREATOR = new Parcelable.Creator<CensusHistory>() {
+        @Override
+        public CensusHistory createFromParcel(Parcel in) {
+            return new CensusHistory(in);
+        }
+
+        @Override
+        public CensusHistory[] newArray(int size) {
+            return new CensusHistory[size];
+        }
+    };
+}
---
+++ Stately/app/src/main/java/com/lloydtorres/stately/dto/CensusHistoryPoint.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2016 Lloyd Torres
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.lloydtorres.stately.dto;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+/**
+ * Created by Lloyd on 2016-04-10.
+ * This is a single point in a census scale's history.
+ */
+@Root(name="POINT", strict=false)
+public class CensusHistoryPoint implements Parcelable {
+
+    @Element(name="TIMESTAMP")
+    public long timestamp;
+    @Element(name="SCORE")
+    public float score;
+
+    public CensusHistoryPoint() { super(); }
+
+    protected CensusHistoryPoint(Parcel in) {
+        timestamp = in.readLong();
+        score = in.readFloat();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(timestamp);
+        dest.writeFloat(score);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<CensusHistoryPoint> CREATOR = new Parcelable.Creator<CensusHistoryPoint>() {
+        @Override
+        public CensusHistoryPoint createFromParcel(Parcel in) {
+            return new CensusHistoryPoint(in);
+        }
+
+        @Override
+        public CensusHistoryPoint[] newArray(int size) {
+            return new CensusHistoryPoint[size];
+        }
+    };
+}
