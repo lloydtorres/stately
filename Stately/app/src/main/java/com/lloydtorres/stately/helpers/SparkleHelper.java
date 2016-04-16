b80a84e3250d68b3f2f75750af22043b82f6fbb5
lloydtorres <lloyd.torres95@gmail.com>
2016-04-16 00:19:52 -0400

    Bump up API version to 8

--- Stately/app/src/main/java/com/lloydtorres/stately/helpers/SparkleHelper.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/helpers/SparkleHelper.java
@@ -124,7 +124,7 @@ public class SparkleHelper {
     // Whitelisted protocols
     public static final String[] PROTOCOLS = {"http", "https", EXPLORE_PROTOCOL};
     // Current NationStates API version
-    public static final String API_VERSION = "7";
+    public static final String API_VERSION = "8";
     // NationStates API
     public static final String DOMAIN_URI = "nationstates.net";
     public static final String BASE_URI = "https://www." + DOMAIN_URI + "/";
