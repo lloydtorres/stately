85a3477e6a575930c16837d8a7687d72aa8047ae
lloydtorres <lloyd.torres95@gmail.com>
2016-04-27 18:06:48 -0400

    Move telegram convo query to onResume
    This refreshes the convo whenever a user returns to this activity (e.g.
    after replying).

--- Stately/app/src/main/java/com/lloydtorres/stately/telegrams/TelegramReadActivity.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/telegrams/TelegramReadActivity.java
@@ -101,8 +101,6 @@ public class TelegramReadActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-
-        startQueryTelegramConvo();
     }
 
     private void setToolbar(Toolbar t) {
@@ -222,6 +220,12 @@ public class TelegramReadActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        startQueryTelegramConvo();
     }
 
     @Override
