12b6bc578da673e577ffcd4f1f3083a6274fb507
lloydtorres <lloyd.torres95@gmail.com>
2017-03-12 23:40:46 -0400

    Even better method of transforming input to ID

--- Stately/app/src/main/java/com/lloydtorres/stately/helpers/SparkleHelper.java
+++ Stately/app/src/main/java/com/lloydtorres/stately/helpers/SparkleHelper.java
@@ -217,8 +217,7 @@ public final class SparkleHelper {
     public static String getNameFromId(String id) {
         if (id != null) {
             // Make sure it's in ~ID form~ first
-            id = id.toLowerCase(Locale.US);
-            id = id.replace(" ", "_");
+            id = getIdFromName(id);
 
             // Split main ID by "_" -- IDs in NationStates have no whitespace, these are replaced by _
             String[] words = id.split("_");
